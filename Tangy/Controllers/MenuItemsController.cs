7770dfe4cfe6eab8f87cbb8c1d25c527e3392756
soumya dwibedy <dwibedy.soumya@gmail.com>
2018-10-09 22:58:03 +0530

    Giving the all project Files

---
+++ Tangy/Controllers/MenuItemsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+using Tangy.Models;
+using Tangy.Models.MenuItemViewModel;
+using Tangy.Utility;
+
+namespace Tangy.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnviroment;
+
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
+        public MenuItemsController(ApplicationDbContext db, IHostingEnvironment hostingEnviroment)
+        {
+            _db = db;
+            _hostingEnviroment = hostingEnviroment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category.ToList(),
+                MenuItem = new Models.MenuItem()
+            };
+        }
+
+        //Get MenuItem
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory);
+
+            return View( await menuItems.ToListAsync());
+        }
+
+        //Create Menu
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST()
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+            if (!ModelState.IsValid)
+                return View();
+            _db.MenuItem.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+            //Image being saved
+            string webRootPath = _hostingEnviroment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            var menuItemFromDB = _db.MenuItem.Find(MenuItemVM.MenuItem.Id);
+            if(files[0]!=null && files[0].Length>0)
+            {
+                var uplods = Path.Combine(webRootPath, "images");
+                var extension = files[0].FileName.Substring(files[0].FileName.LastIndexOf("."), files[0].FileName.Length - files[0].FileName.LastIndexOf("."));
+                using (var filestream = new FileStream(Path.Combine(uplods, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+                menuItemFromDB.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, @"\images\" + SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDB.Image = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public JsonResult GetSubCategory(int CategoryId)
+        {
+            List<SubCategory> subCategories = new List<SubCategory>();
+            subCategories = (from subcategy in _db.SubCategory
+                             where subcategy.CategoryId == CategoryId
+                             select subcategy).ToList();
+            return Json(new SelectList(subCategories, "Id", "Name"));
+        }
+    }
+}
---
+++ Tangy/Controllers/SubCategoriesController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+using Tangy.Models;
+using Tangy.Models.SubCategoryViewModel;
+
+namespace Tangy.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public string StatusMessage { get; set; }
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get ACtion
+        public async Task<IActionResult> Index()
+        {
+            var subCategory = _db.SubCategory.Include(s => s.CategoryPlus);
+            return View(await subCategory.ToListAsync());
+        }
+
+        //Get Action for create
+        public  IActionResult Create()
+        {
+            SubCategoryandCategoryViewModel model = new SubCategoryandCategoryViewModel()
+            {
+                CategoryList =_db.Category.ToList(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = _db.SubCategory.OrderBy(p=>p.Name).Select(p=>p.Name).Distinct().ToList()
+            };
+            return View(model);
+        }
+
+        //Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategoryandCategoryViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name).Count();
+                var doesSubCategoryAndCatExists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name && s.CategoryId == model.SubCategory.CategoryId).Count();
+
+                if (doesSubCategoryExists > 0 && model.isNew)
+                {
+                    //error
+                    StatusMessage = "Error: SubCategory already Exists";
+                }
+                else
+                {
+                    if(doesSubCategoryExists == 0 && !model.isNew)
+                    {
+                        //error
+                        StatusMessage = "Error: SubCategory didn't Exists";
+                    }
+                    else
+                    {
+                        if(doesSubCategoryAndCatExists > 0)
+                        {
+                            //error
+                            StatusMessage = "Error: SubCategory  and Category  combination already Exists";
+                        }
+                        else
+                        {
+                            _db.Add(model.SubCategory);
+                            await _db.SaveChangesAsync();
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+            }
+            SubCategoryandCategoryViewModel modelVm = new SubCategoryandCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToList(),
+                StatusMessage = StatusMessage 
+            };
+            return View(modelVm);
+        }
+
+        //Get Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            var subCategory =  await _db.SubCategory.SingleOrDefaultAsync(m => m.Id == id);
+            if(subCategory==null)
+            {
+                return NotFound();
+            }
+            SubCategoryandCategoryViewModel model = new SubCategoryandCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = subCategory,
+                SubCategoryList = _db.SubCategory.Select(p => p.Name).Distinct().ToList()
+            };
+            return View(model);
+        }
+
+        //Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Edit(int id, SubCategoryandCategoryViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name).Count();
+                var doesSubCategoryAndCatExists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name && s.CategoryId == model.SubCategory.CategoryId).Count();
+                if(doesSubCategoryExists ==0)
+                {
+                    StatusMessage = "Error : SubCategory does not exists. You cannot add a new subcategory here";
+                }
+                else
+                {
+                    if(doesSubCategoryAndCatExists > 0)
+                    {
+                        StatusMessage = "Error : Category and SubCategory combination alreay exists. ";
+                    }
+                    else
+                    {
+                        var subCategoryfromDB = _db.SubCategory.Find(id);
+                        subCategoryfromDB.Name = model.SubCategory.Name;
+                        subCategoryfromDB.CategoryId = model.SubCategory.CategoryId;
+                        await _db.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            SubCategoryandCategoryViewModel modelVM = new SubCategoryandCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = _db.SubCategory.Select(p => p.Name).Distinct().ToList(),
+                StatusMessage = StatusMessage
+            };
+            return View(modelVM);
+
+        }
+
+        //Get Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _db.SubCategory.Include(s => s.CategoryPlus).SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            return View(subCategory);
+        }
+
+        //Get Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _db.SubCategory.Include(s => s.CategoryPlus).SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            return View(subCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subCategory = await _db.SubCategory.SingleOrDefaultAsync(m => m.Id == id);
+            _db.SubCategory.Remove(subCategory);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
